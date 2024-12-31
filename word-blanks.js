08ec6b8266974143e90a19ac78a91d053e61ddb7
tetyana1201 <thretscyn@gmail.com>
2024-12-31 09:15:13 +0200

    39 task

---
+++ word-blanks.js
@@ -0,0 +1,8 @@
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Змініть код лише під цим рядком
+const wordBlanks = "The " + myAdjective + " " + myNoun + " " + myVerb + " very " + myAdverb + " down the street.";
+// Змініть код лише над цим рядком
