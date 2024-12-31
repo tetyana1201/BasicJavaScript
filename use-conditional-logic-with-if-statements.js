e74ee33fb47365e26de33cd5cca63f78c4a900f5
tetyana1201 <thretscyn@gmail.com>
2024-12-31 11:13:42 +0200

    59 task

---
+++ use-conditional-logic-with-if-statements.js
@@ -0,0 +1,12 @@
+function trueOrFalse(wasThatTrue) {
+    // Змініть код лише під цим рядком
+    if (wasThatTrue) {
+        return "Yes, that was true";
+    }
+    return "No, that was false";
+
+    // Змініть код лише над цим рядком
+
+}
+trueOrFalse(true);
+trueOrFalse(false);
