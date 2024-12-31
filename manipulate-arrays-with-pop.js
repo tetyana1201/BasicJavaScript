82d570bb20073b54d0de6996bdb2b0400df339a9
tetyana1201 <thretscyn@gmail.com>
2024-12-31 09:33:51 +0200

    45 task

---
+++ manipulate-arrays-with-pop.js
@@ -0,0 +1,7 @@
+// Налаштування
+const myArray = [["John", 23], ["cat", 2]];
+
+// Змініть код лише під цим рядком
+const removedFromMyArray = myArray.pop();
+console.log(removedFromMyArray);
+console.log(myArray);
